dd5dd8aef332c4140e0ad35928fc9a69b68ff6ba
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2016-05-04 20:02:41 +0300

    Module 1. Task 1. Subtask 2. Fixed shipment setup due Crew changes at previous commits.

--- Module1/Task1/Subtask2/Scripts/ShipmentSetup.sql
+++ Module1/Task1/Subtask2/Scripts/ShipmentSetup.sql
@@ -4,15 +4,14 @@ GO
 declare @FromDate date = '2010-01-01'
 declare @ToDate date = '2015-12-31'
 
-INSERT INTO [dbo].[Shipment] (TruckId, RouteId, DepartureDate)
+INSERT INTO [dbo].[Shipment] (CrewId, RouteId, DepartureDate)
     SELECT TOP 1000 
-		Truck.Id AS TruckId,
+		Crew.Id AS CrewId,
 		Route.Id AS RouteId,
 		dateadd(day, rand(checksum(newid())) * (1 + datediff(day, @FromDate, @ToDate)), @FromDate) AS DepartureDate
 	FROM (
-		SELECT TOP 30 *
-		FROM [dbo].[Truck]
-		ORDER BY NEWID()) as Truck
+		SELECT *
+		FROM [dbo].[Crew]) as Crew
 	CROSS JOIN (SELECT TOP 100 *
 				FROM [dbo].[Route]
 				ORDER BY NEWID()) as Route
