5346c6bf787571518faef7ab61d9fcb915c55a67
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2016-05-05 09:58:21 +0300

    Module 1. Task 1. Subtask 3. Added view as CTE.

--- Module1/Task1/Subtask3/Scripts/ShipmentView.sql
+++ Module1/Task1/Subtask3/Scripts/ShipmentView.sql
@@ -42,45 +42,65 @@ AS
 			DestinationCity.Name, 
 			TruckTable.Brand,
 			TruckTable.FuelConsumption
+GO
 
-
---;WITH cte AS
---(
---   SELECT 
---   	ShipmentTable.DepartureDate AS ShipmentDepartureDate, 
---	ShipmentTable.DeliveryDate AS ShipmentDeliveryDate, 
---	Route.Distance AS Distance, 
---	SourceCity.Name AS SourceCity, 
---	DestinationCity.Name AS DestinationCity,
---	TruckTable.Brand AS Brand,
---	--CargoTable.Id AS CargoId,
---	--ShipmentTable.Id AS ShipmentId
---	SUM(CargoTable.Weight) AS TotalWeight,
---	SUM(CargoTable.Volume) AS TotalVolume,
---         ROW_NUMBER() OVER (PARTITION BY
---			ShipmentTable.Id
---			ORDER BY ShipmentTable.Id
---		) AS rn
---		FROM [dbo].[Shipment] AS ShipmentTable
---		JOIN [dbo].[Route] AS Route
---		ON ShipmentTable.Id = Route.Id
---		JOIN [dbo].[Warehouse] AS SourceWarehouse
---		ON Route.SourceId = SourceWarehouse.Id
---			JOIN [dbo].[City] AS SourceCity
---			ON SourceWarehouse.CityId = SourceCity.Id 
---		JOIN [dbo].[Warehouse] AS DestinationWarehouse
---		ON Route.DestinationId = DestinationWarehouse.Id
---			JOIN [dbo].[City] AS DestinationCity
---			ON DestinationWarehouse.CityId = DestinationCity.Id
---		JOIN [dbo].[Crew] AS CrewTable
---		ON ShipmentTable.CrewId = CrewTable.Id
---			JOIN [dbo].[Truck] AS TruckTable
---			ON TruckTable.Id = CrewTable.Id
---		JOIN [dbo].[CargoShipment] AS CargoShipmentTable
---		ON CargoShipmentTable.ShipmentId = ShipmentTable.Id
---		JOIN [dbo].[Cargo] AS CargoTable
---		ON CargoShipmentTable.CargoId = CargoTable.Id
---)
---SELECT *
---FROM cte
---WHERE rn = 1
+-- CTE (Better way)
+CREATE VIEW [dbo].[ShipmentInfoCTE]
+AS 
+WITH cte AS
+(
+	SELECT 
+	ShipmentTable.DepartureDate AS ShipmentDepartureDate, 
+	ShipmentTable.DeliveryDate AS ShipmentDeliveryDate, 
+	RouteTable.Distance AS Distance, 
+	SourceCity.Name AS SourceCity, 
+	DestinationCity.Name AS DestinationCity,
+	TruckTable.Brand AS Brand,
+	SUM(CargoTable.Weight) AS TotalWeight,
+	SUM(CargoTable.Volume) AS TotalVolume,
+	TruckTable.FuelConsumption*RouteTable.Distance/100  AS TotalFuel,
+	ROW_NUMBER() OVER (PARTITION BY
+		ShipmentTable.Id
+		ORDER BY ShipmentTable.Id
+	) AS rn
+	FROM [dbo].[Shipment] AS ShipmentTable
+	JOIN [dbo].[Route] AS RouteTable
+	ON ShipmentTable.Id = RouteTable.Id
+	JOIN [dbo].[Warehouse] AS SourceWarehouse
+	ON RouteTable.SourceId = SourceWarehouse.Id
+		JOIN [dbo].[City] AS SourceCity
+		ON SourceWarehouse.CityId = SourceCity.Id 
+	JOIN [dbo].[Warehouse] AS DestinationWarehouse
+	ON RouteTable.DestinationId = DestinationWarehouse.Id
+		JOIN [dbo].[City] AS DestinationCity
+		ON DestinationWarehouse.CityId = DestinationCity.Id
+	JOIN [dbo].[Crew] AS CrewTable
+	ON ShipmentTable.CrewId = CrewTable.Id
+		JOIN [dbo].[Truck] AS TruckTable
+		ON TruckTable.Id = CrewTable.Id
+	JOIN [dbo].[CargoShipment] AS CargoShipmentTable
+	ON CargoShipmentTable.ShipmentId = ShipmentTable.Id
+	JOIN [dbo].[Cargo] AS CargoTable
+	ON CargoShipmentTable.CargoId = CargoTable.Id
+	GROUP BY 
+		ShipmentTable.DepartureDate, 
+		ShipmentTable.DeliveryDate, 
+		RouteTable.Distance, 
+		SourceCity.Name, 
+		DestinationCity.Name, 
+		TruckTable.Brand,
+		TruckTable.FuelConsumption,
+		ShipmentTable.Id
+)
+SELECT 
+	ShipmentDepartureDate, 
+	ShipmentDeliveryDate, 
+	Distance, 
+	SourceCity, 
+	DestinationCity,
+	Brand,
+	TotalWeight,
+	TotalVolume,
+	TotalFuel
+FROM cte
+WHERE rn = 1
