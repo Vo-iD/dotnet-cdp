b208f00bb24bd6a4e10223532247333982d6a53e
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2016-07-01 11:12:49 +0300

    Module 3. Practical lesson 2. Added work on lesson.

---
+++ Module3/Practical Lesson 01.07/SecondPracticalLesson/GildedRose/GildedRoseTests.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class GildedRoseTests
+    {
+        private GildedRose _gildedRose;
+
+        [Test]
+        public void Should_Keep_Quality_In_Range_Zero_To_Fifty()
+        {
+            IList<Item> items = TestDataContainer.GetDefaultItemList();
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose);
+
+            foreach (var item in items)
+            {
+                Assert.IsTrue(item.Quality >= 0 && item.Quality <= 50);
+            }
+        }
+
+        [Test]
+        public void Should_Degrade_Qulatity_Twice_As_Fast_After_Passed_Sell_By_Date_For_Aged_Brie()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBrieOnly();
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 4);
+
+            Assert.AreEqual(15, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Increase_Quality_For_Aged_Brie_The_Older_It_Gets()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBrieOnly();
+            _gildedRose = new GildedRose(items);
+
+            _gildedRose.UpdateQuality();
+            var firstDayQuality = items.First().Quality;
+
+            _gildedRose.UpdateQuality();
+            var secondDayQuality = items.First().Quality;
+
+            Assert.IsTrue(secondDayQuality > firstDayQuality);
+        }
+
+        [Test]
+        public void Should_Increase_Quality_For_Backstage_Passes_The_Older_It_Gets()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBackstagePasses();
+            _gildedRose = new GildedRose(items);
+
+            _gildedRose.UpdateQuality();
+            var firstDayQuality = items.First().Quality;
+
+            _gildedRose.UpdateQuality();
+            var secondDayQuality = items.First().Quality;
+
+            Assert.IsTrue(secondDayQuality > firstDayQuality);
+        }
+
+        [Test]
+        public void Should_Increase_Quality_For_Backstage_By_Two_If_Days_Are_In_Range_Five_To_Ten()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBackstagePasses();
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 12);
+
+            Assert.AreEqual(24, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Increase_Quality_For_Backstage_By_Three_If_Days_Are_In_Range_Zero_To_Five()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBackstagePasses();
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 16);
+
+            Assert.AreEqual(33, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Dropt_Quality_To_Zero_For_Backstage_After_The_Concert()
+        {
+            IList<Item> items = TestDataContainer.GetListWithAgedBackstagePasses();
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 21);
+
+            Assert.AreEqual(0, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Not_Change_Quality_For_Sulfuras()
+        {
+            IList<Item> items = TestDataContainer.GetListWithSulfurasOnly();
+            var startedQuality = items.First().Quality;
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose);
+
+            Assert.AreEqual(startedQuality, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Decrease_Days_For_Not_Specified_Item()
+        {
+            IList<Item> items = TestDataContainer.GetListWithoutSpecifiedItems();
+            var startedSellIn = items.First().SellIn;
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 1);
+
+            Assert.AreEqual(startedSellIn - 1, items.First().SellIn);
+        }
+
+        [Test]
+        public void Should_Decrease_Quality_For_Not_Specified_Item()
+        {
+            IList<Item> items = TestDataContainer.GetListWithoutSpecifiedItems();
+            var startedQuality = items.First().Quality;
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 1);
+
+            Assert.AreEqual(startedQuality - 1, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Decrease_Quality_For_Not_Specified_Item_If_SellIn_Less_Than_Zero()
+        {
+            IList<Item> items = TestDataContainer.GetListWithoutSpecifiedItemsWithZeroSellIn();
+            _gildedRose = new GildedRose(items);
+            var expectedQuality = items.First().Quality - 2;
+
+            UpdateQuality(_gildedRose, 1);
+
+            Assert.AreEqual(expectedQuality, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Decrease_Quality_By_Two_Each_Day_If_Sell_In_More_Than_Zero()
+        {
+            IList<Item> items = TestDataContainer.GetListWithConjuredOnly();
+            var expectedQuality = items.First().Quality - 2;
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 1);
+
+            Assert.AreEqual(expectedQuality, items.First().Quality);
+        }
+
+        [Test]
+        public void Should_Decrease_Quality_By_Four_Each_Day_If_Sell_In_Less_Than_Zero()
+        {
+            IList<Item> items = TestDataContainer.GetListWithConjuredOnlyWithZeroSellIn();
+            var expectedQuality = items.First().Quality - 4;
+            _gildedRose = new GildedRose(items);
+
+            UpdateQuality(_gildedRose, 1);
+
+            Assert.AreEqual(expectedQuality, items.First().Quality);
+        }
+
+        private void UpdateQuality(GildedRose gildedRose, int times = 50)
+        {
+            for (int t = 0; t < times; t++)
+            {
+                gildedRose.UpdateQuality();
+            }
+        }
+    }
+}
---
+++ Module3/Practical Lesson 01.07/SecondPracticalLesson/GildedRose/TestDataContainer.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public static class TestDataContainer
+    {
+        public static IList<Item> GetDefaultItemList()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 50},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithAgedBrieOnly()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 3, Quality = 10}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithSulfurasOnly()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 10}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithConjuredOnly()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithConjuredOnlyWithZeroSellIn()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Conjured Mana Cake", SellIn = 0, Quality = 6}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithAgedBackstagePasses()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10}
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetListWithoutSpecifiedItems()
+        {
+            return new List<Item>
+            {
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            };
+        }
+
+        public static IList<Item> GetListWithoutSpecifiedItemsWithZeroSellIn()
+        {
+            return new List<Item>
+            {
+                new Item {Name = "Elixir of the Mongoose", SellIn = 0, Quality = 7},
+            };
+        }
+    }
+}
