5ec177e69182f0444f5810604b8cf01b95b8de3b
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2016-07-05 08:35:07 +0300

    Module 3. Task Kata 1. Added solution. Done first two steps. Also has been moved GildedRose kata to new folder.

---
+++ Module3/Kata 1. String Calculator/StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace StringCalculatorKata
+{
+    class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var result = numbers.Split(',').Sum(x => int.Parse(x));
+
+            return result;
+        }
+    }
+}
---
+++ Module3/Kata 1. String Calculator/StringCalculatorKata/StringCalculatorKata/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void Should_Return_Zero_If_Was_Empty_String()
+        {
+            Assert.AreEqual(0, _calculator.Add(string.Empty));
+        }
+
+        [Test]
+        public void Should_Return_Number_If_Was_Single_Number()
+        {
+            Assert.AreEqual(5, _calculator.Add("5"));
+        }
+
+        [Test]
+        public void Should_Return_Sum_Of_Two_Numbers()
+        {
+            Assert.AreEqual(6, _calculator.Add("5,1"));
+        }
+
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("0,0,0,0,0", 0)]
+        [TestCase("0,0,0,5,0", 5)]
+        [TestCase("0,0,0,5,0,1,1,1,1,1", 10)]
+        public void Should_Return_Sum_Of_Unkown_Amount_Of_Numbers(string input, int expected)
+        {
+            Assert.AreEqual(expected, _calculator.Add(input));
+        }
+    }
+}
